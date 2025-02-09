a63df90bf3a3ea71b2d103b37173b6471f4a74fb
Tahirnawaz <tahirnawazwebpro@gmail.com>
2025-02-09 16:30:35 +0500

    update token security

--- Application/Services/AuthService/TokenService.cs
+++ Application/Services/AuthService/TokenService.cs
@@ -11,39 +11,15 @@ namespace E_Commerce.Application.Services.AuthService
     public class TokenService : ITokenService
     {
         private readonly IConfiguration _configuration;
-
         public TokenService(IConfiguration configuration)
         {
             _configuration = configuration;
         }
 
-        //public string GenerateJwtToken(ApplicationUser user)
-        //{
-        //    var SecurityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
-        //    var Credentials = new SigningCredentials(SecurityKey, SecurityAlgorithms.HmacSha256);
-
-        //    var claims = new[]
-        //    {
-        //        new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-        //        new Claim(ClaimTypes.Name, user.FullName),
-        //        new Claim(ClaimTypes.Email,user.Email),
-        //        new Claim(ClaimTypes.Role,user.Role.ToLower()),
-        //    };
-
-        //    var token = new JwtSecurityToken
-        //        (
-        //        //_configuration["Jwt:Issuer"],
-        //        //_configuration["Jwt:Audience"],
-        //        claims:claims,
-        //        signingCredentials: Credentials,
-        //        expires: DateTime.UtcNow.AddHours(1)
-        //        );
-
-        //    return new JwtSecurityTokenHandler().WriteToken(token);
-        //}
-
         public string GenerateJwtToken(ApplicationUser user)
         {
+            var jwtsettings = _configuration.GetSection("Jwt");
+            var tokenExpiry = jwtsettings.GetValue<int>("TokenExpiry");
             var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:Key"]));
             var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
 
@@ -56,8 +32,10 @@ namespace E_Commerce.Application.Services.AuthService
             };
 
             var token = new JwtSecurityToken(
+                //_configuration["Jwt:Issuer"],
+               //_configuration["Jwt:Audience"],
                 claims: claims,
-                expires: DateTime.UtcNow.AddHours(1),
+                expires: DateTime.UtcNow.AddHours(tokenExpiry),
                 signingCredentials: credentials
             );
 
